f441c2ce6c75a12e76a06e87e8287ecbc4f4e54b
Lu Khei <kheiboardwarrior@gmail.com>
2020-10-27 19:33:14 +0800

    Move images to static folder to be served
    implement random generator and use handlebar for layout and display of dice results and images

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-const express = require('express')
-const hbs = require('express-handlebars')
-const app = express()
+const express = require("express");
+const hbs = require("express-handlebars");
+const app = express();
 
-app.engine('hbs', hbs({defaultLayout: 'default.hbs'}))
-app.set('view engine', 'hbs')
+app.engine("hbs", hbs({ defaultLayout: "default.hbs" }));
+app.set("view engine", "hbs");
 
-const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000;
+const PORT =
+  parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000;
 
 app.use(express.static(__dirname + "/static"));
 
-app.get('/roll', (req,res) => {
-    res.status(200)
-    res.type('text/html')
-    res.render('dice')
-})
+app.get("/roll", (req, res) => {
+  const diceNum = [1, 2, 3, 4, 5, 6];
+  const imgPath = [
+    "/images/dado-1.png",
+    "/images/roll2.png",
+    "/images/three_dots.png",
+    "/images/four.png",
+    "/images/Five-Image.png",
+    "/images/dice-showing-6.png",
+  ];
 
+  const firstRandom = diceNum[Math.floor(Math.random() * diceNum.length)];
+  const secondRandom = diceNum[Math.floor(Math.random() * diceNum.length)];
+  res.status(200);
+  res.type("text/html");
+  res.render("dice", {
+    randomNumOne: firstRandom,
+    randomNumTwo: secondRandom,
+    imgNumOne: imgPath[firstRandom - 1],
+    imgNumTwo: imgPath[secondRandom - 1],
+  });
+});
 
-
-
-app.use((req,res) => {
-    res.redirect("/")
-})
+app.use((req, res) => {
+  res.redirect("/");
+});
 
 app.listen(PORT, () => {
-    console.log(`Hello ${PORT} has started`)
-})
+  console.log(`Hello ${PORT} has started`);
+});
